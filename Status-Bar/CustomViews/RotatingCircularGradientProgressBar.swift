ff6f5379d9587df722d744b2117574e476bea3aa
levi davis <ldd235@gmail.com>
2020-12-10 15:47:26 -0500

    Adds rotating gradient circular progress bar

--- Status-Bar/CustomViews/RotatingCircularGradientProgressBar.swift
+++ Status-Bar/CustomViews/RotatingCircularGradientProgressBar.swift
@@ -1,5 +1,5 @@
 //
-//  GradientCircularProgressBar.swift
+//  RotatingCircularGradientProgressBar.swift
 //  Status-Bar
 //
 //  Created by Levi Davis on 12/10/20.
@@ -8,22 +8,23 @@
 import UIKit
 
 @IBDesignable
-class GradientCircularProgressBar: UIView {
+class RotatingCircularGradientProgressBar: UIView {
     @IBInspectable var color: UIColor = .gray {
         didSet {
             setNeedsDisplay()
         }
     }
-    
     @IBInspectable var gradientColor: UIColor = .white {
         didSet {
             setNeedsDisplay()
         }
     }
-    
-    @IBInspectable var ringWidth: CGFloat = 15.0
-    
-    var progress: CGFloat = 0.5 {
+    @IBInspectable var ringWidth: CGFloat = 15 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    var progress: CGFloat = 5 {
         didSet {
             setNeedsDisplay()
         }
@@ -32,7 +33,7 @@ class GradientCircularProgressBar: UIView {
     private let progressLayer = CAShapeLayer()
     private let backgroundMask = CAShapeLayer()
     private let gradientLayer = CAGradientLayer()
-        
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupLayers()
@@ -48,7 +49,7 @@ class GradientCircularProgressBar: UIView {
     override func draw(_ rect: CGRect) {
         let circlePath = UIBezierPath(ovalIn: rect.insetBy(dx: ringWidth / 2, dy: ringWidth / 2))
         backgroundMask.path = circlePath.cgPath
-               
+        
         progressLayer.path = circlePath.cgPath
         progressLayer.lineCap = .round
         progressLayer.strokeStart = 0
@@ -58,7 +59,7 @@ class GradientCircularProgressBar: UIView {
         gradientLayer.frame = rect
         gradientLayer.colors = [color.cgColor, gradientColor.cgColor, color.cgColor]
     }
-
+    
     private func setupLayers() {
         backgroundMask.lineWidth = ringWidth
         backgroundMask.fillColor = nil
@@ -69,24 +70,28 @@ class GradientCircularProgressBar: UIView {
         progressLayer.fillColor = nil
         
         layer.addSublayer(gradientLayer)
-        layer.transform = CATransform3DMakeRotation(CGFloat(90 * Double.pi / 180), 0, 0, -1)
         
         gradientLayer.mask = progressLayer
         gradientLayer.locations = [0.35, 0.5, 0.65]
     }
     
     private func createAnimation() {
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotationAnimation.fromValue = CGFloat(Double.pi / 2)
+        rotationAnimation.toValue = CGFloat(2.5 * Double.pi)
+        rotationAnimation.duration = 4
+        
+        gradientLayer.add(rotationAnimation, forKey: "rotationAnimation")
+        
         let startPointAnimation = CAKeyframeAnimation(keyPath: "startPoint")
         startPointAnimation.values = [CGPoint.zero, CGPoint(x: 1, y: 0), CGPoint(x: 1, y: 1)]
         
-        startPointAnimation.isRemovedOnCompletion = false
         startPointAnimation.repeatCount = Float.infinity
         startPointAnimation.duration = 1
         
         let endPointAnimation = CAKeyframeAnimation(keyPath: "endPoint")
         endPointAnimation.values = [CGPoint(x: 1, y: 1), CGPoint(x: 0, y: 1), CGPoint.zero]
         
-        endPointAnimation.isRemovedOnCompletion = false
         endPointAnimation.repeatCount = Float.infinity
         endPointAnimation.duration = 1
         

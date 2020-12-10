d28d53b8b3c182b05c70b2be2c808ef1989f90dc
levi davis <ldd235@gmail.com>
2020-12-10 16:45:41 -0500

    Adds VC transition

--- Status-Bar/SecondViewController.swift
+++ Status-Bar/SecondViewController.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "hand.thumbsup.fill")
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        createImageView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createImageView() {
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        [imageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+         imageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+         imageView.heightAnchor.constraint(equalToConstant: 250),
+         imageView.widthAnchor.constraint(equalToConstant: 250)].forEach {$0.isActive = true}
     }
-    */
-
 }
--- Status-Bar/ViewController.swift
+++ Status-Bar/ViewController.swift
@@ -28,10 +28,18 @@ class ViewController: UIViewController {
         gradientHorizontalProgressBarOutlest.progress = CGFloat(sender.value)
         gradientCircularProgressBarOutlet.progress = CGFloat(sender.value)
         rotatingCircularGradientProgressBarOutlet.progress = CGFloat(sender.value)
+        
+        if sender.value == 1.0 {
+            transitionWithAnimation()
+        }
     }
     
     private func transitionWithAnimation() {
+        let secondVC = SecondViewController()
+        secondVC.modalPresentationStyle = .overFullScreen
         
+        UIView.transition(from: self.view, to: secondVC.view, duration: 2.0, options: .transitionCrossDissolve) { [weak self] _ in
+            self?.present(secondVC, animated: true, completion: nil)
+        }
     }
 }
-

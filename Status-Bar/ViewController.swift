6a4a87bdc349920749fd01e7a98ec0a2e3bfd9b4
levi davis <ldd235@gmail.com>
2020-12-10 10:41:30 -0500

    Initial Commit

---
+++ Status-Bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Status-Bar
+//
+//  Created by Levi Davis on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
